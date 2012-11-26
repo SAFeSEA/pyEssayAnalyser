1fbca699d41f6ed825373806df707f2c557d3fa8
vanch3d <nicolas.vanlabeke@open.ac.uk>
2012-11-26 14:27:43 +0000

    Added logger and cache directory

---
+++ src/EssayAnalyser/__init__.py
@@ -0,0 +1,24 @@
+import logging
+from logging import getLogger
+import tempfile
+
+
+# Create an event logger for the EssayAnalyser module
+_apiLogger = getLogger(__name__)
+_apiLogger.setLevel(logging.DEBUG)
+
+# Create a file handler for the output
+fh = logging.FileHandler('_EssayAnalyser.log')
+fh.setLevel(logging.INFO)
+
+# Change the format of the output
+formatter = logging.Formatter('%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s')
+fh.setFormatter(formatter)
+
+# Add the file handler to the logger
+_apiLogger.addHandler(fh)
+
+# Create a temporary directory (used for caching results)
+# This will be created once per session, when the system is launched 
+_tempDir = tempfile.mkdtemp(suffix='', prefix='AAA_essayAnalyser_cache_', dir=None)
+_apiLogger.info("Create cache directory: " + _tempDir) 
